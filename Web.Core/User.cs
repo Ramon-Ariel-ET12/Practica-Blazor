0bc433b9523d1a849c38c39716b625a8b52e8bf3
Ramon-Ariel-ET12 <Ramonarielet12d1@gmail.com>
2024-05-20 02:29:30 -0300

    Updated

--- Web.Core/User.cs
+++ Web.Core/User.cs
@@ -1,54 +1,53 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Web.Core
+namespace Web.Core;
+
+[Table("User")]
+public class User
 {
-    [Table("User")]
-    public class User
+    [Key]
+    [Required]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Indica que la base de datos generará automáticamente el valor
+    public int Id { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string? Name { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string? Surname { get; set; }
+
+    [Required]
+    public DateTime Birthdate { get; set; }
+
+    [Required]
+    [StringLength(15)]
+    public string? Sex { get; set; }
+
+    [Required]
+    [StringLength(30)]
+    public string? Nationality { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string? Email { get; set; }
+
+    [Required]
+    public string? Password { get; set; }
+
+    // Constructor vacío requerido por Entity Framework Core
+    public User() {}
+
+    // Constructor con parámetros
+    public User(string name, string surname, DateTime birthday, string sex, string nationality, string email, string password)
     {
-        [Key]
-        [Required]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Indica que la base de datos generará automáticamente el valor
-        public int Id { get; set; }
-        [Required]
-        [StringLength(50)]
-        public string? Name { get; set; }
-
-        [Required]
-        [StringLength(50)]
-        public string? Surname { get; set; }
-
-        [Required]
-        public string? Birthdate { get; set; }
-
-        [Required]
-        [StringLength(1)]
-        public string? Sex { get; set; }
-
-        [Required]
-        [StringLength(30)]
-        public string? Nationality { get; set; }
-
-        [Required]
-        [StringLength(100)]
-        public string? Email { get; set; }
-
-        [Required]
-        public string? Password { get; set; }
-
-        // Constructor vacío requerido por Entity Framework Core
-        public User() {}
-
-        // Constructor con parámetros
-        public User(string name, string surname, string birthday, string sex, string nationality, string email, string password)
-        {
-            Name = name;
-            Surname = surname;
-            Birthdate = birthday;
-            Sex = sex;
-            Nationality = nationality;
-            Email = email;
-            Password = password;
-        }
+        Name = name;
+        Surname = surname;
+        Birthdate = birthday;
+        Sex = sex;
+        Nationality = nationality;
+        Email = email;
+        Password = password;
     }
 }
